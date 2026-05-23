f92c39afa57fa906b35edba4d21444b3f3d88a53
Mike Hall <mike@puddingtime.org>
2026-05-22 18:24:56 -0700

    Initial cask for asystem-lsp

---
+++ Casks/asystem-lsp.rb
@@ -0,0 +1,23 @@
+cask "asystem-lsp" do
+  version "0.2.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://dist.puddingtime.net/asystem-lsp/v#{version}/asystem-lsp_#{version}_aarch64.tar.gz"
+  name "asystem-lsp"
+  desc "LSP server for asystem wikilinks in Markdown (queries asystem-service)"
+  homepage "https://github.com/mph-asystem/asystem-lsp"
+
+  depends_on arch: :arm64
+
+  binary "asystem-lsp"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{staged_path}/asystem-lsp"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/.asystem-lsp.json"
+  ]
+end
